22dc9c9df56f957b0e155843ca48355d3846f9ac
EDI <EDI@EDI-NITRO5>
2026-03-04 15:09:42 +0200

    Add project files.

---
+++ BizSecureDemo22180087/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using BizSecureDemo22180087.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BizSecureDemo22180087.Controllers;
+
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly AppDbContext _db;
+    public HomeController(AppDbContext db) => _db = db;
+
+    /*     public async Task<IActionResult> Index()
+        {
+            var uid = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+            var orders = await _db.Orders
+                .Where(o => o.UserId == uid)
+                .OrderByDescending(o => o.Id)
+                .ToListAsync();
+
+            return View(orders);
+        }
+    }
+    */
+
+
+    public async Task<IActionResult> Index()
+    {
+        var uid = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!); //Взима ID-то на логнатия потребител
+
+        var myOrders = await _db.Orders
+            .Where(o => o.UserId == uid)
+            .OrderByDescending(o => o.Id)
+            .ToListAsync(); // Чете поръчките от базата спрямо логнатия потребител
+
+        var allOrders = await _db.Orders
+            .OrderByDescending(o => o.Id)
+            .ToListAsync(); // Чете всички поръчки от базата
+
+        ViewBag.AllOrders = allOrders; //Подава всички поръчки към View-то през ViewBag. ViewBag е „чанта“ за допълнителни данни към View-то. Така View-то може да показва и публичен списък от поръчки.
+        return View(myOrders);
+    }
+}
---
+++ BizSecureDemo22180087/ViewModels/CreateOrderVm.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BizSecureDemo22180087.ViewModels;
+public class CreateOrderVm
+{
+    [Required, MaxLength(80)]
+    public string Title { get; set; } = "";
+    [Required]
+    public decimal Amount { get; set; }
+}
